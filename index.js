3f0e141d17c81d0e21824bcbee984d08f761bab6
Cyprian <lightningcyranus@gmail.com>
2025-05-29 07:40:47 +0200

    created function to write cards and counted from given number

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+function writeMessages(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// function to count down from a given number
+function countDown(start) {
+  while (start >= 0) {
+    console.log(start);
+    start--;
+  }
+}
+
+// Test the functions
+console.log(writeCards(["Guadalupe", "Ollie", "Aki", "Maya", "Sam"], "birthday"));
+console.log(writeMessages(writeCards, "birthday"));
+countDown(10);
+
+
